be513de050c4f3c8063a3b56f55a1c34635a15dc
Vihanga01 <vihanga.laksahan@gmail.com>
2021-09-25 19:47:47 +0530

    customer prof up

---
+++ app/src/main/java/com/example/mobilehouse/Customer_profile_up.java
@@ -0,0 +1,14 @@
+package com.example.mobilehouse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Customer_profile_up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_profile_up);
+    }
+}
